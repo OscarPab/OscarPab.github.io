e7679cbfa505540054cdfc9ff995a871ca805c00
OscarPab <oscaripingui@gmail.com>
2025-09-22 03:43:53 -0600

    Mi portafolio listo para Vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true
+  },
+  // IMPORTANTE: Para dominio raíz (oscarpab.github.io)
+  basePath: '',
+  assetPrefix: '',
+  trailingSlash: false,
+};
 
 export default nextConfig;
